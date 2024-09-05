c2f62f741e62bdda23c72b6b5259e300c072be49
AshutoshSharma608 <rs4030536@gmail.com>
2024-09-05 14:53:11 +0000

    Container config done

---
+++ .devcontainer/01_basics/03_ConversionOperation.js
@@ -0,0 +1,23 @@
+/*let score = 33
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInString = String(score)
+console.log(typeof valueInString)*/
+
+
+let score = "33ash"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number(score)
+console.log(typeof score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// score = null >= 0
+// score = undefined >= NaN
+// score = string >= NaN
+// score = 
