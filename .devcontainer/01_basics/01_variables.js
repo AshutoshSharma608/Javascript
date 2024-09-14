3ae088da1d8d04d9bb94ee376d26a8b250526165
AshutoshSharma608 <rs4030536@gmail.com>
2024-09-14 06:28:49 +0000

    container config

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 157
+// const account_id = 157
+// const account_Id = 157
+
+let accountEmail = "ash4030536@gmail.com"
+var accountPassword = "123"
+accountCity = "indore"
+let accountState;
+// accountId = 234   not allowed
+
+accountEmail= "rs4924@gmail.com"
+accountPassword= "546"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+console.log([accountId,accountPassword,accountEmail,accountCity,accountState]);
+
+
+//console.log([accountId],[accountPassword],[accountEmail],[accountCity])
+/*
+prefer not to use var 
+because of block scope and functional scope
+
+*/
---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer version of javascript 
+
+// alert (3+3) we are using nodejs not browser
+
+let name = "ashutosh" // string
+let age = 18           // number
+let isLoggedIn = false  // boolean
+let state;  // undefined
+
+// number => 2 to power 53
+// bigInt
+// string => ""
+// boolean = > true/false
+// null = standalone value
+// undefined
+// symbol = unique
+// object
+
+console.log(typeof "ashutosh")
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
