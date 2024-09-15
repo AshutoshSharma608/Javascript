b7e2c35fd239de507609d0e2deb1670916cac274
AshutoshSharma608 <rs4030536@gmail.com>
2024-09-15 06:12:00 +0000

    conversion of operation

--- .devcontainer/01_basics/03_ConversionOperation.js
+++ .devcontainer/01_basics/03_ConversionOperation.js
@@ -1,13 +1,14 @@
-/*let score = 33
+let score = 33
 
 console.log(typeof score)
 console.log(typeof(score));
 
 let valueInString = String(score)
-console.log(typeof valueInString)*/
+console.log(typeof valueInString)
+console.log(valueInString)
 
 
-let score = "33ash"
+/*let score = "33ash"
 
 console.log(typeof score)
 console.log(typeof(score))
@@ -19,5 +20,16 @@ console.log(valueInNumber)
 
 // score = null >= 0
 // score = undefined >= NaN
-// score = string >= NaN
-// score = 
+// score = string >= NaN                             // node 03_ConversionOperation.js
+// score = true =1 , false = 0
+// 33 = 33
+// 33abc = NaN*/
+
+let isLoggedIn = "0"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+// 1=> true,  0 => false/ true
+// "" => false
+// "ashu" => true
